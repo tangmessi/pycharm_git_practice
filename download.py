e47abbb62a4233ff53d5ef8c7922267d72faf09c
TANGGY666 <664068464@qq.com>
2021-05-17 23:31:56 +0800

    下载url里的文件

---
+++ download.py
@@ -0,0 +1,3 @@
+import urllib.request as url_r
+url_r.urlretrieve('http://music.163.com/song/media/outer/url?id=3986992.mp3','yellow.mp3',None)
+'http://music.163.com/song/media/outer/url?id=3986992.mp3'
