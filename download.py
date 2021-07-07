4894bd03c49439a0024637d942cef9c9b8e06e6c
TANGGY666 <664068464@qq.com>
2021-07-08 00:54:00 +0800

    。。

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
 import urllib.request as url_r
 url_r.urlretrieve('http://music.163.com/song/media/outer/url?id=3986992.mp3','yellow.mp3',None)
 'http://music.163.com/song/media/outer/url?id=3986992.mp3'
+"http://music.163.com/song/media/outer/url?id=381962.mp3"
